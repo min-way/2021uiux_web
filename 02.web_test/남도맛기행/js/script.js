e5cdb647448d62cf0efefdaa1b92600e20d929e4
min-way <nissyou@naver.com>
2021-04-04 22:46:05 +0900

    -

---
+++ 02.web_test/남도맛기행/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+$(".gnb>li").hover(function(){
+    $(this).find("ul.sub").stop().slideDown();
+},function(){
+    $(this).find("ul.sub").stop().slideUp();
+    });
+    
+$(".contents .slide li").eq(0).siblings().css({"left":"-800px"});
+    var slideI=0;
+    
+    setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+        $(".contents .slide li").eq(slideI).siblings().animate({"left":"-800px"},500);
+        $(".contents .slide li").eq(slideI).animate({"left":"0"},500);
+        
+        
+    },3000);
+$(".notice_gal h2").click(function(){
+    $(this).addClass("on").siblings().removeClass("on");
+    $(this).next("ul").addClass("on");
+});
+    
+$(".notice li").eq(0).click(function(){
+    $(".modal").fadeIn();
+});
+
+    $("button").click(function(){
+       $(".modal").fadeOut();
+    });
+    
+}); //end
