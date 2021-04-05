35e41df3df0f43115daddbbf4b156e80be68f492
min-way <nissyou@naver.com>
2021-04-05 18:21:41 +0900

    -

--- 02.web_test/남도맛기행/js/script.js
+++ 02.web_test/남도맛기행/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
+//.gnb>li에 hover 했을때 ul.sub 가 천천히 나타나기
 $(".gnb>li").hover(function(){
     $(this).find("ul.sub").stop().slideDown();
 },function(){
     $(this).find("ul.sub").stop().slideUp();
-    });
-    
+});
+//.contents .slide li 좌에서 우로 슬라이드 되며 3초마다 이동
 $(".contents .slide li").eq(0).siblings().css({"left":"-800px"});
     var slideI=0;
     
@@ -14,22 +16,22 @@ $(".contents .slide li").eq(0).siblings().css({"left":"-800px"});
         }else{
             slideI=0;
         }
-        $(".contents .slide li").eq(slideI).siblings().animate({"left":"-800px"},500);
-        $(".contents .slide li").eq(slideI).animate({"left":"0"},500);
-        
+    $(".contents .slide li").eq(slideI).siblings().animate({"left":"-800px"},500);
+    $(".contents .slide li").eq(slideI).animate({"left":"0"},500);
         
     },3000);
+//.notice_gal h2 클릭시 탭 메뉴 클릭 시 해당 ul이 보여주기
 $(".notice_gal h2").click(function(){
     $(this).addClass("on").siblings().removeClass("on");
     $(this).next("ul").addClass("on");
 });
-    
+//.notice li .modal 첫 번째 클릭시 팝업창 띄우기
 $(".notice li").eq(0).click(function(){
     $(".modal").fadeIn();
 });
-
-    $("button").click(function(){
-       $(".modal").fadeOut();
-    });
+//button 클릭시 .modal 팝업창 닫기
+$("button").eq(0).click(function(){
+    $(".modal").fadeOut();
+});
     
 }); //end
