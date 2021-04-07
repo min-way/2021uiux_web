c8befa39b1450be143d4f234309628214300c2df
min-way <nissyou@naver.com>
2021-04-07 10:21:09 +0900

    -

--- 02.web_test/대한은행/js/script.js
+++ 02.web_test/대한은행/js/script.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-    $(".gnb>ul").hover(function(){
-        $(this).find(".sub_menu").stop().slideDown();
-    },function(){
-        $(this).find(".sub_menu").stop().slideUp();
-    });
-    
-=======
-    $(".gnb>li").hover(function(){
-        $(".sub_menu").stop().slideDown();
-    },function(){
-        $(".sub_menu").stop().slideUp();
-    });
->>>>>>> 5823c00b3bb6c92bea31bb492f9e7da8a9a659fc
+
 });
